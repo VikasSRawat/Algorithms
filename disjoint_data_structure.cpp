dae892b96be05dc69c8e084d177ee175ea0342e3
VikasSRawat <90858879+VikasSRawat@users.noreply.github.com>
2023-09-07 13:33:05 +0530

    Update disjoint_data_structure.cpp

--- disjoint_data_structure.cpp
+++ disjoint_data_structure.cpp
@@ -1,58 +1,47 @@
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
-
-
-int findpar(int x,int *parent){
-    if (parent[x] != x) 
-        parent[x] = findpar(parent[x],parent);// we will keep on recursively call findpar until we find the parent of the set and update the current nodes parent hence this is also called path compression
- 
-    return parent[x];// If the node is itself the parent then we have reached the parent of the vertice and returns that node
-}
-
-void union_set(int u,int v,int *parent,int *rank_set){
-    u=findpar(u,parent);
-    v=findpar(v,parent);
-    // Getting the parent of each node/vertex and comparing the ranks of each vertice
-
-    // Smaller rank sets gets absorbed by the bigger rank sets
-    if(rank_set[u]>rank_set[v])
-        parent[v]=u;
-    else if(rank_set[u]<rank_set[v])
-        parent[v]=u;
-    //If the rank of both the sets are same then 
-    else{
-        parent[v]=u;
-        rank_set[u]++;// Incrementing the rank of the parent of the new set
+class DisjointSet{
+    vector<int> rank,parent;
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1,0);
+        for(int i = 0 ; i <= n ; i++)
+            parent[i] = i;
     }
-}
 
-
-int main()
-{
-    int *rank_set;
-    int *parent;
-    int n=6;
-
-    parent=new int[n+1];
-    rank_set=new int[n+1];
-
-    for(int i=1;i<=n;i++){
-        rank_set[i]=0;//setting the rank of every vertice as 0
-        parent[i]=i;//initially every vertice is their own parent
+    int findpar(int node){
+        if(node == parent[node])// Ultimate parent is itself hence returning that node
+            return node;
+        return parent[node] = findpar(parent[node]);// Here we are updating each parent node along the path to the ultimate parent to reduce time complexit
     }
-    union_set(1,2,parent,rank_set);
-    union_set(3,2,parent,rank_set);
-    union_set(4,5,parent,rank_set);
-    union_set(5,6,parent,rank_set);
-
-    for(int i=1;i<=n;i++)
-        cout<<parent[i]<<" -> "<<i<<endl;
-
-    if(findpar(3,parent)!=findpar(1,parent))
-        cout<<"Not same components"<<endl;
+    void unionByRank(int u,int v){
+        int ult_pu = findpar(u);
+        int ult_pv = findpar(v);
+        if(ult_pu == ult_pv)//belonging to the same component;
+            return;
+        if(rank[ult_pu] > rank[ult_pv]){
+            parent[ult_pv] = ult_pu;
+        }
+        else if(rank[ult_pv] > rank[ult_pu]){
+            parent[ult_pu] = ult_pv;
+        }
+        else{
+            parent[ult_pv] = ult_pv;
+            rank[ult_pu]++;
+    }
+};
+int main(){
+    DisjointSet ds(7);
+    ds.unionByRank(1,2);
+    ds.unionByRank(2,3);
+    ds.unionByRank(4,5);
+    ds.unionByRank(6,7);
+    ds.unionByRank(5,6);
+    // checking if 3 and 7 are part of the same component or not 
+    if(ds.findpar(3) == ds.findpar(7))
+        cout<<"Same component"<<endl;
     else
-        cout<<"Same components"<<endl;
+        cout<<"Not Same component"<<endl;
     
     return 0;
 }
