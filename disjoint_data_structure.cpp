cf2525db34444d9b3a870f33c3f15704846e5c67
VikasSRawat <90858879+VikasSRawat@users.noreply.github.com>
2023-09-18 13:07:21 +0530

    Update disjoint_data_structure.cpp

--- disjoint_data_structure.cpp
+++ disjoint_data_structure.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class DisjointSet{
     vector<int> rank,parent;
     DisjointSet(int n){
-        rank.resize(n+1,0);
+        rank.resize(n+1,0); // initially all the ranks will be the same due to the fact that all are componenets themselves
         parent.resize(n+1,0);
         for(int i = 0 ; i <= n ; i++)
             parent[i] = i;
@@ -12,10 +12,12 @@ class DisjointSet{
     int findpar(int node){
         if(node == parent[node])// Ultimate parent is itself hence returning that node
             return node;
-        return parent[node] = findpar(parent[node]);// Here we are updating each parent node along the path to the ultimate parent to reduce time complexit
+        return parent[node] = findpar(parent[node]);// Here we are updating each parent node along the path to the ultimate parent to reduce time complexity
     }
     void unionByRank(int u,int v){
-        int ult_pu = findpar(u);
+        // We find the ultimate parent of both the vertices and check if they belong to the same component i.e., same ultimate parent or not
+        // If not then we just check the ranks 
+        int ult_pu = findpar(u); 
         int ult_pv = findpar(v);
         if(ult_pu == ult_pv)//belonging to the same component;
             return;
@@ -26,6 +28,7 @@ class DisjointSet{
             parent[ult_pu] = ult_pv;
         }
         else{
+            // if both ultimate parents have the same rank then we just join one component to another(your choice) and increase the other componenet's ultimate parent rank
             parent[ult_pv] = ult_pu;
             rank[ult_pu]++;
         }
